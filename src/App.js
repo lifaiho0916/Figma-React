b68d797ca55407e9b2f6dfa8e7e86b3f79f64f7e
Monirul Islam <Md Monirul Islam>
2022-12-16 12:16:15 +0600

    header menu basic design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { MessageIcon, NotifiactionIcon } from "./svgs";
-import "./App.css"
+import './App.css';
+import Content from './components/Content/Content';
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="app">
-      <div className="header">
+    <div className='bg-black h-screen'>
+      <Header />
+      <div className='flex'>
+        <div className='w-3/12'>
+          <Sidebar />
+        </div>
+        <div className='w-9/12'>
+          <Content />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Header/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FaGripLines } from 'react-icons/fa';
+import logoImg from '../../assets/images/logo.png';
+
+function Logo() {
+  return (
+    <div className='flex w-3/12 items-center justify-between px-3'>
+      <div className='h-16 w-32'>
+        <img src={logoImg} className='h-full w-full object-contain' alt='' />
+      </div>
+      <p className='text-2xl text-white'>
+        <FaGripLines />
+      </p>
+    </div>
+  );
+}
+
+export default Logo;
